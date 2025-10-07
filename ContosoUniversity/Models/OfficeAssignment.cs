3170185e9f1b2a57067e69f309d3746b8c8b82a6
Viktor080181 <lozhnvikt0801@mail.ru>
2025-10-07 22:11:27 +0300

    Scafold all models

---
+++ ContosoUniversity/Models/OfficeAssignment.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class OfficeAssignment
+    {
+        [Key]
+        public int InstructorID { get; set; }
+        [StringLength(100)]
+        [DisplayName("Office Location")]
+
+        public string Location { get; set; }
+        public Instructor Instructor { get; set; }
+    }
+}
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,4 +1,6 @@
 ﻿
+using Microsoft.SqlServer.Server;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -7,10 +9,27 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        [DisplayName("Фамилия")]
+        [StringLength(24)]
+        [RegularExpression(@"^[A-Z]+[a-z]*$")]
         public string LastName { get; set; }
+        [DisplayName("Имя")]
+        [StringLength(24)]
+        [RegularExpression(@"^[A-Z]+[a-z]*$")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name="Дата поступления")]
+       
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy.MM.dd}", ApplyFormatInEditMode = true)]
+
         public DateTime EnrollmentDate { get; set; }
 
+        
+        [DisplayName("Студент")]
+       
+        public string FullName { get => $"{LastName} {FirstName}"; }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
